5df148f13d51d2963a5f18e1996f07a3b93b7427
Chris Kaktsis <christoskaktsis@gmail.com>
2025-10-01 13:10:11 +0300

    first commit

---
+++ Blueprint/App/BlueprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueprintApp.swift
+//  Blueprint
+//
+//  Created by Christos Kaktsis on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BlueprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
